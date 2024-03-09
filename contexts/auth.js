9aa4b9e086dc5be1244e24585e907323e665a297
Andrea Riley <andreariley@me.com>
2024-03-09 14:52:34 -0500

    testing a login fix again

--- contexts/auth.js
+++ contexts/auth.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState } from 'react';
 import jwt from 'jsonwebtoken';
 const baseUrl = process.env.NEXT_PUBLIC_API_URL;
-const tokenUrl = baseUrl + 'api/token/';
+const tokenUrl = baseUrl + '/api/token/';
 
 const AuthContext = createContext();
 
